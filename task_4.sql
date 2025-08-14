d84e211da7ffb99a2a2bbea59049361c4e43848c
dev chris <99187522+devchris12@users.noreply.github.com>
2025-08-14 15:13:15 +0100

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,11 +1,3 @@
--- =============================================
--- task_4.sql - Full Description of Books Table
--- Prints the full description of the books table
--- Database name will be passed as argument to mysql command
--- =============================================
+-- Script to print the full description of the books table from alx_book_store database
 
--- Use the alx_book_store database
-USE alx_book_store;
-
--- Show full description of the books table
 SHOW COLUMNS FROM books;
