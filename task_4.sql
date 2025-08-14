919f98c5cdca90d04423397c6d8f078f794ae5c3
dev chris <99187522+devchris12@users.noreply.github.com>
2025-08-14 15:29:31 +0100

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,3 @@
--- Script to print the full description of the books table from alx_book_store database
+USE alx_book_store;
 
-SHOW COLUMNS FROM books;
+SHOW COLUMNS FROM books;-- Script to print the full description of the books table from alx_book_store database
