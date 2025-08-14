3bd21934e9294d2ffd7a278f45b29db854b1ff7d
dev chris <99187522+devchris12@users.noreply.github.com>
2025-08-14 14:56:38 +0100

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,18 +1,1 @@
--- Script to print the full description of the books table from alx_book_store database
--- Using INFORMATION_SCHEMA to get table structure without DESCRIBE or EXPLAIN
-SHOW CREATE TABLE `books`;
-
-SELECT 
-    COLUMN_NAME AS 'Field',
-    COLUMN_TYPE AS 'Type',
-    IS_NULLABLE AS 'Null',
-    COLUMN_KEY AS 'Key',
-    COLUMN_DEFAULT AS 'Default',
-    EXTRA AS 'Extra'
-FROM 
-    INFORMATION_SCHEMA.COLUMNS 
-WHERE 
-    TABLE_SCHEMA = 'alx_book_store' 
-    AND TABLE_NAME = 'books'
-ORDER BY 
-    ORDINAL_POSITION;
+SHOW COLUMNS FROM books;
