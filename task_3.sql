a99a61711358ff779f93762fd32430d71231a848
dev chris <99187522+devchris12@users.noreply.github.com>
2025-08-14 14:36:06 +0100

    Update task_3.sql

---
+++ task_3.sql
@@ -0,0 +1,11 @@
+-- =============================================
+-- task_3.sql - List All Tables
+-- Lists all tables in the alx_book_store database
+-- Database name will be passed as argument to mysql command
+-- =============================================
+
+-- Use the alx_book_store database
+USE alx_book_store;
+
+-- List all tables in the alx_book_store database
+SHOW TABLES;
