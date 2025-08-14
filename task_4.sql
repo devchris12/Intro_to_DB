fda0d358268a8f3414e42dd3ce2ff48855aa3886
dev chris <99187522+devchris12@users.noreply.github.com>
2025-08-14 14:48:16 +0100

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,14 +1,1 @@
--- task_4.sql
-
-SELECT 
-    COLUMN_NAME AS 'Field',
-    COLUMN_TYPE AS 'Type',
-    IS_NULLABLE AS 'Null',
-    COLUMN_KEY AS 'Key',
-    COLUMN_DEFAULT AS 'Default',
-    EXTRA AS 'Extra'
-FROM 
-    INFORMATION_SCHEMA.COLUMNS
-WHERE 
-    TABLE_SCHEMA = 'alx_book_store' 
-    AND TABLE_NAME = 'books';
+SHOW CREATE TABLE `books`;
