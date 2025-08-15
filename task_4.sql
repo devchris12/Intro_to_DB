5d82607d25eda95c7a595ff500a90fb1545d5633
dev chris <99187522+devchris12@users.noreply.github.com>
2025-08-15 14:24:31 +0100

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,14 @@
-USE alx_book_store;
-
-SHOW COLUMNS FROM books;-- Script to print the full description of the books table from alx_book_store database
+SELECT 
+    COLUMN_NAME AS 'Field',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Null',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = DATABASE()
+    AND TABLE_NAME = 'books'
+ORDER BY 
+    ORDINAL_POSITION;
